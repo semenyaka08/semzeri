3f5289848674f4177ffa199c874b753629ab4482
Даник <pistriuga.danylo@lll.kpi.ua>
2025-06-03 22:06:24 +0300

    Implement AlgorithmRepository

--- backend/src/Semzeri.DAL/Repositories/AlgorithmRepository.cs
+++ backend/src/Semzeri.DAL/Repositories/AlgorithmRepository.cs
@@ -1,17 +1,32 @@
-﻿using Semzeri.DAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Semzeri.DAL.Entities;
 using Semzeri.DAL.Repositories.Interfaces;
+using Semzeri.DAL.UnitOfWork;
 
 namespace Semzeri.DAL.Repositories;
 
-public class AlgorithmRepository : IAlgorithmRepository
+public class AlgorithmRepository (ApplicationDbContext context, IUnitOfWork unitOfWork) : IAlgorithmRepository
 {
-    public Task UpdateAlgorithm(string title, string description)
+    public async Task UpdateAlgorithm(string title, string description)
     {
-        throw new NotImplementedException();
+        var algo = await context.Algorithms.FirstOrDefaultAsync();
+
+        if (algo is null)
+        {
+            algo = new Algorithm
+            {
+                Title = title,
+                Description = description
+            };
+            await context.Algorithms.AddAsync(algo);
+            await unitOfWork.SaveChangesAsync();
+            return;
+        }
+
+        algo.Description = description;
+        algo.Title = title;
+        await unitOfWork.SaveChangesAsync();
     }
 
-    public Task<Algorithm?> GetAlgorithm()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<Algorithm?> GetAlgorithm() => await context.Algorithms.FirstOrDefaultAsync();
 }
