9a91068bd4ee56b3ae422db8aff3ea5b1a001b83
Даник <pistriuga.danylo@lll.kpi.ua>
2025-06-03 22:16:49 +0300

    Implement UrlsRepository

--- backend/src/Semzeri.DAL/Repositories/UrlsRepository.cs
+++ backend/src/Semzeri.DAL/Repositories/UrlsRepository.cs
@@ -1,48 +1,75 @@
-﻿using Semzeri.DAL.DTOs.Urls;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Semzeri.DAL.DTOs.Urls;
 using Semzeri.DAL.Entities;
 using Semzeri.DAL.Repositories.Interfaces;
+using Semzeri.DAL.UnitOfWork;
 
 namespace Semzeri.DAL.Repositories;
 
-public class UrlsRepository : IUrlsRepository
+public class UrlsRepository (ApplicationDbContext context, IUnitOfWork unitOfWork) : IUrlsRepository
 {
-    public Task<UrlInfo> AddUrlAsync(UrlInfo urlInfo)
+    public async Task<UrlInfo> AddUrlAsync(UrlInfo urlInfo)
     {
-        throw new NotImplementedException();
+        await context.UrlInfos.AddAsync(urlInfo);
+
+        await unitOfWork.SaveChangesAsync();
+
+        return urlInfo;
     }
 
-    public Task<UrlInfo?> GetUrlByIdAsync(Guid id)
+    public async Task<UrlInfo?> GetUrlByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        return await context.UrlInfos.FirstOrDefaultAsync(x=>x.Id == id);
     }
 
-    public Task<UrlInfo?> GetUrlByOriginalUrlAsync(string originalUrl)
+    public async Task<UrlInfo?> GetUrlByOriginalUrlAsync(string originalUrl)
     {
-        throw new NotImplementedException();
+        return await context.UrlInfos.FirstOrDefaultAsync(x=>x.OriginalUrl == originalUrl);
     }
 
-    public Task<bool> IsCodeAlreadyExist(string code)
+    public async Task<bool> IsCodeAlreadyExist(string code)
     {
-        throw new NotImplementedException();
+        return await context.UrlInfos.AnyAsync(x=>x.Code == code);
     }
 
-    public Task<UrlInfo?> GetUrlByCodeAsync(string code)
+    public async Task<UrlInfo?> GetUrlByCodeAsync(string code)
     {
-        throw new NotImplementedException();
+        return await context.UrlInfos.FirstOrDefaultAsync(x=>x.Code == code);
     }
 
-    public Task<List<string>> GetAllCodesAsync()
+    public async Task<List<string>> GetAllCodesAsync()
     {
-        throw new NotImplementedException();
+        return await context.UrlInfos.Select(z => z.Code).ToListAsync();
     }
 
-    public Task<(IEnumerable<UrlInfo>, int)> GetUrlsAsync(UrlsGetRequest request, string userEmail)
+    public async Task<(IEnumerable<UrlInfo>, int)> GetUrlsAsync(UrlsGetRequest request, string userEmail)
     {
-        throw new NotImplementedException();
+        var query = context.UrlInfos.Where(z => (request.SearchParam == null
+                                                 || z.Id.ToString() == request.SearchParam)
+                                                && z.UserEmail == userEmail);
+
+        int totalCount = await query.CountAsync();
+        
+        query = request.SortDirection == "asc" ? query.OrderBy(GetSelectorKey(request.SortBy)) : query.OrderByDescending(GetSelectorKey(request.SortBy));
+
+        return (await query.Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).ToListAsync(), totalCount);
     }
 
-    public Task DeleteUrlAsync(UrlInfo urlInfo)
+    public async Task DeleteUrlAsync(UrlInfo urlInfo)
     {
-        throw new NotImplementedException();
+        context.UrlInfos.Remove(urlInfo);
+
+        await unitOfWork.SaveChangesAsync();
+    }
+    
+    private Expression<Func<UrlInfo, object>> GetSelectorKey(string? sortItem)
+    {
+        return sortItem switch
+        {
+            "date" => z => z.CreatedAt,
+            "id" => z => z.Id,
+            _ => z => z.Id
+        };
     }
 }
