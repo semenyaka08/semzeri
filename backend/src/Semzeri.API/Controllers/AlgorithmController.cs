2f29ab91a6f959a51ad27b2c45f115044f519e1e
semenyaka08 <Serh.semka@gmail.com>
2025-06-04 17:17:21 +0300

    Add AlgorithmController for managing algorithm updates and retrieval

---
+++ backend/src/Semzeri.API/Controllers/AlgorithmController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Semzeri.BusinessLogic.DTOs.Algorithm;
+using Semzeri.BusinessLogic.Services.Interfaces;
+using Semzeri.DAL;
+
+namespace Semzeri.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AlgorithmController(IAlgorithmService algorithmService) : ControllerBase
+{
+    [Authorize(Roles = ApplicationRoles.Admin)]
+    [HttpPost]
+    public async Task<IActionResult> UpdateAlgorithm([FromBody] UpdateAlgorithmRequest request)
+    {
+        await algorithmService.UpdateAlgorithm(request);
+
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAlgorithm()
+    {
+        var algorithm = await algorithmService.GetAlgorithm();
+
+        return Ok(algorithm);
+    }
+}
