49af2859b1207c4d87eaacebfeda1d9e91bb4da2
semenyaka08 <Serh.semka@gmail.com>
2025-06-04 17:15:32 +0300

    Add AdminController for managing URLs with authorization

---
+++ backend/src/Semzeri.API/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Semzeri.BusinessLogic.DTOs.Admin;
+using Semzeri.BusinessLogic.Services.Interfaces;
+using Semzeri.DAL;
+
+namespace Semzeri.API.Controllers;
+
+public class AdminController (IUrlsService urlsService) : ControllerBase
+{
+    [HttpGet("urls")]
+    [Authorize(Roles = ApplicationRoles.Admin)]
+    public async Task<IActionResult> GetAllUrls([FromQuery] AdminUrlsGetRequest request)
+    {
+        var urls = await urlsService.GetAllUrlsAsync(request);
+
+        return Ok(urls);
+    }
+}
