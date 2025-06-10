339c5efba6e8af22d1bf5bd3732ad6d1db15024c
semenyaka08 <Serh.semka@gmail.com>
2025-06-10 20:28:44 +0300

    Update CORS policy to remove trailing slash from allowed origins

--- backend/src/Semzeri.API/Program.cs
+++ backend/src/Semzeri.API/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200", "https://semzeri.azurewebsites.net/"));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200", "https://semzeri.azurewebsites.net"));
 
 app.UseAuthentication();
 app.UseAuthorization();
